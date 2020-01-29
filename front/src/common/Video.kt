ccebd636a15e938e2350711edf2c6fb4eaa23bc1
Kongus <kongus99@gmail.com>
2020-01-29 14:45:03 +0100

    cleanup

--- front/src/common/Video.kt
+++
@@ -1,5 +0,0 @@
-package common
-
-data class Video(val id: Long, val title: String, val speaker: String, val videoUrl: String) {
-    constructor(id: Long, title: String, videoUrl: String) : this(id, title, "", videoUrl)
-}
