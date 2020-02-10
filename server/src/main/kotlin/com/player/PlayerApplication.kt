655d24e3fc57950992ee8c1f4062deaaf5ae6bb8
Kongus <kongus99@gmail.com>
2020-02-10 14:03:30 +0100

    split run config to deploy and local

--- server/src/main/kotlin/com/player/PlayerApplication.kt
+++ server/src/main/kotlin/com/player/PlayerApplication.kt
@@ -11,22 +11,26 @@ import java.net.Proxy
 
 @SpringBootApplication
 class PlayerApplication {
+    val host = System.getenv()["http.proxyHost"] ?: System.getenv()["https.proxyHost"]
+    val port = System.getenv()["http.proxyPort"] ?: System.getenv()["https.proxyPort"]
+
+
     @get:Bean
     val restTemplate: RestTemplate
         get() {
             val template = RestTemplate()
-            val factory = SimpleClientHttpRequestFactory()
-            val address = InetSocketAddress("10.144.1.10", 8080)
-//            val proxy = Proxy(Proxy.Type.HTTP, address)
-//            factory.setProxy(proxy)
-            template.requestFactory = factory
+            if (host != null) {
+                val factory = SimpleClientHttpRequestFactory()
+                factory.setProxy(Proxy(Proxy.Type.HTTP, InetSocketAddress(host!!, Integer.parseInt(port!!))))
+                template.requestFactory = factory
+            }
             return template
         }
+
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            System.getenv().toList().sortedBy { it.first.toString() }.forEach { println(it) }
             SpringApplication.run(PlayerApplication::class.java, *args)
         }
     }
