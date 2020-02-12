00f76bd66ff2e6fcf607054bb8c38b9422604a43
Kongus <kongus99@gmail.com>
2020-02-12 15:19:02 +0100

    Edit modal upgraded

--- elm/src/Video/Edit.elm
+++ elm/src/Video/Edit.elm
@@ -2,22 +2,25 @@ module Video.Edit exposing (..)
 
 import Bootstrap.Button as Button
 import Bootstrap.ButtonGroup as ButtonGroup
+import Bootstrap.Form as Form
 import Bootstrap.Form.Input as Input
-import Bootstrap.Form.InputGroup as InputGroup
 import Bootstrap.Modal as Modal
 import Extra exposing (isJust)
-import Html exposing (Html, br, div, text)
+import Html exposing (Html, div, text)
+import Process
 import RemoteData exposing (WebData)
+import Task
+import Url
 import Video.Meta as Meta exposing (Meta)
 import Video.Video as Video exposing (Video)
 
 
 type alias Model =
-    { visible : Modal.Visibility, submitted : Bool, video : Maybe Video }
+    { visible : Modal.Visibility, submitted : Bool, url : String, videoId : Maybe String, vid : Maybe Video }
 
 
 init =
-    { visible = Modal.hidden, submitted = False, video = Nothing }
+    { visible = Modal.hidden, submitted = False, url = "", videoId = Nothing, vid = Nothing }
 
 
 resetSubmitted model =
@@ -26,7 +29,14 @@ resetSubmitted model =
 
 setVideo : Model -> Video -> Model
 setVideo model video =
-    { model | video = Just video }
+    { model
+        | url =
+            Video.toUrl video.videoId
+                |> Maybe.map Url.toString
+                |> Maybe.withDefault ""
+        , videoId = Just video.videoId
+        , vid = Just video
+    }
 
 
 type Msg
@@ -35,7 +45,8 @@ type Msg
     | Submit
     | Submitted (WebData (Maybe Int))
     | Verified String (WebData Meta)
-    | UpdateUrl String
+    | ChangeUrl String
+    | VerifyUrl ()
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -54,31 +65,39 @@ update msg model =
         Submit ->
             let
                 cmd =
-                    model.video
+                    model.vid
                         |> Maybe.map
                             (\v ->
                                 case v.id of
                                     Nothing ->
-                                        Video.post Submitted v
+                                        Video.post Submitted v |> Debug.log "POST"
 
                                     _ ->
-                                        Video.put Submitted v
+                                        Video.put Submitted v |> Debug.log "PUT"
                             )
                         |> Maybe.withDefault Cmd.none
             in
             ( model, cmd )
 
-        UpdateUrl url ->
+        ChangeUrl url ->
+            let
+                parsed =
+                    Video.parseId url
+            in
+            ( { model | url = url, videoId = parsed, vid = Maybe.andThen (\_ -> model.vid) parsed }
+            , Process.sleep 1000 |> Task.perform VerifyUrl
+            )
+
+        VerifyUrl _ ->
             ( model
-            , url
-                |> Video.parseId
+            , model.videoId
                 |> Maybe.map
                     (\u -> Meta.get u (Verified u))
                 |> Maybe.withDefault Cmd.none
             )
 
         Submitted _ ->
-            ( { model | visible = Modal.hidden, submitted = True, video = Nothing }, Cmd.none )
+            ( { model | visible = Modal.hidden, submitted = True, vid = Nothing }, Cmd.none )
 
         Verified videoId webData ->
             let
@@ -90,7 +109,7 @@ update msg model =
                         _ ->
                             Nothing
             in
-            ( { model | video = video }, Cmd.none )
+            ( { model | vid = video }, Cmd.none )
 
 
 addButton : (Msg -> a) -> Html a
@@ -103,43 +122,70 @@ editButton video mapper =
     ButtonGroup.button [ Button.info, Button.small, Button.onClick (mapper (Open (Just video))) ] [ text "Edit" ]
 
 
+validation model =
+    if String.trim model.url |> String.isEmpty then
+        ( Input.attrs [], Input.attrs [], div [] [] )
+
+    else if not <| isJust model.videoId then
+        ( Input.attrs [], Input.danger, Form.invalidFeedback [] [ text <| "No video id found in " ++ model.url ] )
+
+    else if not <| isJust model.vid then
+        ( Input.danger, Input.danger, Form.invalidFeedback [] [ text <| "Incorrect video id " ++ Maybe.withDefault "" model.videoId ] )
+
+    else
+        ( Input.success, Input.success, Form.validFeedback [] [ text <| "Video found for id " ++ Maybe.withDefault "" model.videoId ] )
+
+
 modal : (Msg -> msg) -> Model -> Html msg
 modal mapper model =
     let
         name =
-            model.video |> Maybe.map .title |> Maybe.withDefault ""
+            model.vid |> Maybe.map .title |> Maybe.withDefault ""
 
-        url =
-            model.video
-                |> Maybe.map .videoId
-                |> Maybe.withDefault ""
+        ( upperStatus, lowerStatus, lowerFeedback ) =
+            validation model
     in
     div []
         [ Modal.config Close
             |> Modal.large
             |> Modal.hideOnBackdropClick True
             |> Modal.body []
-                [ InputGroup.config
-                    (InputGroup.text [ Input.disabled True, Input.placeholder "Name", Input.value name ])
-                    |> InputGroup.view
-                , br [] []
-                , InputGroup.config
-                    (InputGroup.text
-                        [ Input.placeholder "Please paste correct youtube url"
-                        , Input.value url
-                        , Input.onInput UpdateUrl
+                [ Form.form []
+                    [ Form.form []
+                        [ Form.group []
+                            [ Form.label [] [ text "Title" ]
+                            , Input.text
+                                [ upperStatus
+                                , Input.readonly True
+                                , Input.placeholder "Name"
+                                , Input.value name
+                                ]
+                            ]
+                        , Form.group []
+                            [ Form.label [] [ text "Url" ]
+                            , Input.text
+                                [ lowerStatus
+                                , Input.placeholder "Please enter correct youtube url"
+                                , Input.value model.url
+                                , Input.onInput ChangeUrl
+                                ]
+                            , lowerFeedback
+                            ]
                         ]
-                    )
-                    |> InputGroup.view
+                    ]
                 ]
             |> Modal.footer []
-                [ Button.submitButton
-                    [ Button.primary
-                    , Button.onClick Submit
-                    , Button.disabled (not <| isJust model.video)
+                (if Extra.isJust model.vid then
+                    [ Button.submitButton
+                        [ Button.primary
+                        , Button.onClick Submit
+                        ]
+                        [ text "Save" ]
                     ]
-                    [ text "Save" ]
-                ]
+
+                 else
+                    []
+                )
             |> Modal.view model.visible
             |> Html.map mapper
         ]
